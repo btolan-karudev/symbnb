72171ecf0cf8bfe4f485dcf098ea1017d1d8203e
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-12-12 15:16:49 +0100

    login logout

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,15 +3,39 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
+     * Permet d afficher et de gerer le formulaire de conection
+     *
      * @Route("/login", name="account_login")
+     *
+     * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
     }
+
+    /**
+     * Permet de se deconecter
+     *
+     * @Route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout()
+    {
+
+    }
+
 }
