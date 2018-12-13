cfa9f1cd991d04ace59ba7e8d3f7a612d0b475ef
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-12-13 01:06:11 +0100

    modif pass ok

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -9,6 +9,7 @@ use App\Form\PasswordUpdateType;
 use App\Form\RegistrationType;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -120,11 +121,41 @@ class AccountController extends AbstractController
      *
      * @return Response
      */
-    public function updatePassword()
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, ObjectManager $manager)
     {
         $passwordUpdate = new PasswordUpdate();
 
+        $user = $this->getUser();
+
         $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 1.Verifier que le mot de passe actuel est le meme que celui du user
+            if (!password_verify($passwordUpdate->getOldPassword(), $user->getHash())) {
+                //Gerer l error
+                $form->get('oldPassword')->addError(
+                    new FormError("Le mot de passe que vous aves reinseigne n ai pas le mot de passe actuel")
+                );
+            } else {
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setHash($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Le nouveau mot de passe a ete enregistre avec succes !"
+                );
+
+                return $this->redirectToRoute('homepage');
+            }
+
+        }
 
         return $this->render('account/password.html.twig', [
             'form' => $form->createView()
