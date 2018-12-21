9a8029207f0b14b7179927c6f88176d92f6d4c78
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-12-21 09:13:33 +0100

    reservation

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -5,18 +5,43 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Entity\Booking;
 use App\Form\BookingType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookingController extends AbstractController
 {
     /**
      * @Route("/ads/{slug}/book", name="create_booking")
+     * @IsGranted("ROLE_USER")
+     *
+     * @param Ad $ad
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function book(Ad $ad)
+    public function book(Ad $ad, Request $request, ObjectManager $manager)
     {
         $booking = new Booking();
         $form = $this->createForm(BookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+
+            $booking
+                ->setBooker($user)
+                ->setAd($ad);
+
+            $manager->persist($booking);
+            $manager->flush();
+
+            return $this->redirectToRoute('booking_show', ['id' => $booking->getId()]);
+        }
 
 
         return $this->render('booking/book.html.twig', [
@@ -24,4 +49,20 @@ class BookingController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * Permet d afficher la page d'une reservation
+     *
+     * @Route("/booking/{id}", name="booking_show")
+     *
+     * @param Booking $booking
+     * @return Response
+     */
+    public function show(Booking $booking)
+    {
+        return $this->render('booking/show.html.twig', [
+            'booking' => $booking
+        ]);
+    }
+
 }
