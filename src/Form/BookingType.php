fa8e941b991c7f3aa01460999c5ccf0b62c5b10e
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-12-22 15:03:54 +0100

    pickup ui

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Booking;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,18 +14,16 @@ class BookingType extends ApplicationType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('startDate', DateType::class,
+            ->add('startDate', TextType::class,
                 $this->getConfiguration(
                     "Date d'arriver",
-                    "La date à laquelle vous compter arriver",
-                    ["widget" => "single_text"]
+                    "La date à laquelle vous compter arriver"
                 )
             )
-            ->add('endDate', DateType::class,
+            ->add('endDate', TextType::class,
                 $this->getConfiguration(
                     "Date de départ",
-                    "la date à laquelle vous quittez les lieux",
-                    ["widget" => "single_text"]
+                    "la date à laquelle vous quittez les lieux"
                 )
             )
             ->add('comment', TextareaType::class,
