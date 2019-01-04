37d47b5a306490f513429baf23223f6c0f5fbdee
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-01-04 14:04:32 +0100

    adit add admin

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminAdController extends AbstractController
@@ -15,6 +19,38 @@ class AdminAdController extends AbstractController
     {
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     *
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     *
+     * @param Ad $ad
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+        }
+
+
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
         ]);
     }
 }
