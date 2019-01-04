098eb165feb23cf2bd1633f5920ccd81a93e683b
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-01-04 04:09:21 +0100

    admin deconnect

--- src/Controller/AdminAccountController.php
+++ src/Controller/AdminAccountController.php
@@ -4,16 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminAccountController extends AbstractController
 {
     /**
      * @Route("/admin/login", name="admin_account_login")
+     *
+     * @param AuthenticationUtils $utils
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('admin/account/login.html.twig', [
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
 
+        return $this->render('admin/account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
         ]);
+    }
+
+    /**
+     * Permet de se deconecter
+     *
+     * @Route("/admin/logout", name="admin_account_logout")
+     *
+     * @return void
+     */
+    public function logout()
+    {
+
     }
 }
