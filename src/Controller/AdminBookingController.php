b5369d534c7e5ae79abccebb562bdbc242a64c38
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-01-08 21:24:38 +0100

    booking controler created

---
+++ src/Controller/AdminBookingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminBookingController extends AbstractController
+{
+    /**
+     * @Route("/admin/bookings", name="admin_booking_index")
+     * @param BookingRepository $repo
+     * @return Response
+     */
+    public function index(BookingRepository $repo)
+    {
+        return $this->render('admin/booking/index.html.twig', [
+            'bookings' => $repo->findAll(),
+        ]);
+    }
+}
