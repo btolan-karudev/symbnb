a434b7463f785c2f7c72e18ed7d89e5fa5bbedac
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-01-09 07:38:32 +0100

    AdminBookingType

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +22,40 @@ class AdminBookingController extends AbstractController
     {
         return $this->render('admin/booking/index.html.twig', [
             'bookings' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * Permet d editer une reservation
+     *
+     * @Route("/admin/bookings/{id}/edit", name="admin_bookings_edit")
+     *
+     * @param Booking $booking
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function edit(Booking $booking, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AdminBookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "La réservation n°{$booking->getId()} a bien été modifiée"
+            );
+
+            return $this->redirectToRoute('admin_booking_index');
+        }
+
+        return $this->render('admin/booking/edit.html.twig', [
+            'form' => $form->createView(),
+            'booking' => $booking
         ]);
     }
 }
