d6b5e65fe97374d36c80d7a5e153aa764428ea16
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-01-10 07:29:58 +0100

    pagination sauf service ok

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -14,18 +14,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminAdController extends AbstractController
 {
     /**
-     * @Route("/admin/ads/{page}", name="admin_ads_index")
+     * @Route("/admin/ads/{page<\d+>?1}", name="admin_ads_index")
      * @param AdRepository $repo
      * @param int $page
      * @return Response
      */
-    public function index(AdRepository $repo, $page = 1)
+    public function index(AdRepository $repo, $page)
     {
         $limit = 10;
         $start = $page * $limit - $limit;
+        $total = count($repo->findAll());
+        $pages = ceil($total / $limit);
 
         return $this->render('admin/ad/index.html.twig', [
-            'ads' => $repo->findBy([], [], $limit, $start)
+            'ads' => $repo->findBy([], [], $limit, $start),
+            'pages' => $pages,
+            'page' => $page
         ]);
     }
 
